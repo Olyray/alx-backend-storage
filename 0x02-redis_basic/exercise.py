339357fd0710f4690fd12adccdf36bbd7f66e384
Olamide <olire96@gmail.com>
2023-07-19 22:07:12 +0100

    Task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,15 +20,17 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(
+        self, key: str, fn: Callable = None
+           ) -> Union[str, bytes, int, float]:
         """Returns the type passed in the callable function"""
         value = self._redis.get(key)
         return fn(value) if fn is not None else value
-    
-    def get_str(self, key: str):
+
+    def get_str(self, key: str) -> str:
         """Calls the get method using a string"""
         return self.get(key, str)
 
-    def get_int(self, key: str):
+    def get_int(self, key: str) -> int:
         """Calls the get method using an int"""
         return self.get(key, int)
