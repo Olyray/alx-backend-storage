9621bf37881a9741006aaeabb9c079ee51d6cdb7
Olamide <olire96@gmail.com>
2023-07-19 22:04:14 +0100

    Task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,16 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Returns the type passed in the callable function"""
+        value = self._redis.get(key)
+        return fn(value) if fn is not None else value
+    
+    def get_str(self, key: str):
+        """Calls the get method using a string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str):
+        """Calls the get method using an int"""
+        return self.get(key, int)
