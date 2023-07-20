52013c2ee6c0cf68eb748ec41f377184ca5dba7e
Olamide <olire96@gmail.com>
2023-07-20 02:35:43 +0100

    The tasks

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
+class OuterClass:
+    def outer_method(self):
+        def inner_method():
+            print(inner_method.__qualname__)
 
-Cache = __import__('exercise').Cache
+        inner_method()
 
-cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+outer = OuterClass()
+outer.outer_method()
